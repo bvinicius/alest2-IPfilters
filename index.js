f73e1b7f220d9063f4ed8afd1a09ce09e62f8517
vbazanella <vinicius.bazanella1@gmail.com>
2020-03-28 14:58:39 -0300

    start ranges

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const fs = require('fs')
+
+for (let i = 12; i <= 12; i ++) {
+    const caso = i < 10 ? ('0' + i) : i.toString()
+    const casefile = `./casos/caso${caso}.txt`
+
+    console.log(`** reading file ${casefile} **`)
+
+    const ranges = fs.readFileSync(casefile)
+        .toString()
+        .split('\n')
+        .filter(e=>e)
+        .map(e => {
+            const range = e.split('-').map(e=>parseInt(e))
+            const first = range[0]
+            const last = range[1]
+        
+            return {first: first, last: last}
+        })
+        .sort((a, b) => a.first - b.first)
+
+    console.log(ranges)
+}
